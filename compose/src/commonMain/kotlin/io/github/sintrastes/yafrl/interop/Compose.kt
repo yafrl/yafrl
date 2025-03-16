09102f8439597053314c79ea83621aa0c3446c34
Nathan Bedell <nbedell@tulane.edu>
2025-03-15 22:12:45 -0400

    Add a utility for getting frame events in compose.

--- compose/src/commonMain/kotlin/io/github/sintrastes/yafrl/interop/Compose.kt
+++ compose/src/commonMain/kotlin/io/github/sintrastes/yafrl/interop/Compose.kt
@@ -1,10 +1,18 @@
 package io.github.sintrastes.yafrl.interop
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.withFrameNanos
+import io.github.sintrastes.yafrl.Event
 import io.github.sintrastes.yafrl.annotations.FragileYafrlAPI
 import io.github.sintrastes.yafrl.State
+import io.github.sintrastes.yafrl.broadcastEvent
 import io.github.sintrastes.yafrl.internal.Timeline
 import kotlinx.coroutines.launch
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.nanoseconds
 
 /**
  * Utility function to assist with using a [io.github.sintrastes.yafrl.State]
@@ -23,4 +31,31 @@ fun <A> State<A>.composeState(): androidx.compose.runtime.State<A> {
     }
 
     return state
+}
+
+/**
+ * Utility to get an every that fires for every frame in a
+ *  Jetpack Compose application.
+ *
+ * Each event consists of the [Duration] of the most recent frame.
+ **/
+@Composable
+fun getFrameClock(): Event<Duration> {
+    val clock = remember { broadcastEvent<Duration>() }
+
+    var lastTime: Long = -1
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            withFrameNanos { time ->
+                if (lastTime > 0) {
+                    clock.send((time - lastTime).nanoseconds)
+                }
+
+                lastTime = time
+            }
+        }
+    }
+
+    return clock
 }
