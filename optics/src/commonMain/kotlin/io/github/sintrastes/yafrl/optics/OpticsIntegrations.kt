cd110c946b71fafee7271626c7a1ea9e8cf817b7
Nathan Bedell <nbedell@tulane.edu>
2025-03-27 19:25:40 -0400

    Add a focus operator.

--- optics/src/commonMain/kotlin/io/github/sintrastes/yafrl/optics/OpticsIntegrations.kt
+++ optics/src/commonMain/kotlin/io/github/sintrastes/yafrl/optics/OpticsIntegrations.kt
@@ -1,7 +1,9 @@
 package io.github.sintrastes.yafrl.optics
 
+import arrow.optics.Lens
 import arrow.optics.Prism
 import io.github.sintrastes.yafrl.Event
+import io.github.sintrastes.yafrl.State
 
 /** Embed an event in a larger set of events via a [Prism]. */
 fun <A, B> Event<A>.embed(inclusion: Prism<B, A>): Event<B> {
@@ -9,3 +11,11 @@ fun <A, B> Event<A>.embed(inclusion: Prism<B, A>): Event<B> {
         inclusion.reverseGet(it)
     }
 }
+
+/** Focs on a smaller part of a [State] by applying a [Lens]. */
+fun <A, B> State<A>.focus(lens: Lens<A, B>): State<B> {
+    return map {
+        lens.get(it)
+    }
+}
+
