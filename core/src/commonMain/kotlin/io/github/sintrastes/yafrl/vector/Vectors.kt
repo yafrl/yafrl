5c0df7164be7f3e7cc5762e484b2afbcce611e47
Nathan Bedell <nbedell@tulane.edu>
2025-03-26 20:44:35 -0400

    Add documentation for vector types.

--- core/src/commonMain/kotlin/io/github/sintrastes/yafrl/vector/Vectors.kt
+++ core/src/commonMain/kotlin/io/github/sintrastes/yafrl/vector/Vectors.kt
@@ -1,9 +1,13 @@
 package io.github.sintrastes.yafrl.vector
 
+/** A two-dimensional vector in Cartesian coordinates using [Float] values. */
 data class Float2(val x: Float, val y: Float)
 
+/** A three-dimensional vector in Cartesian coordinates using [Float] values. */
 data class Float3(val x: Float, val y: Float, val z: Float)
 
+/** A two-dimensional vector in Cartesian coordinates using [Double] values. */
 data class Double2(val x: Double, val y: Double)
 
+/** A three-dimensional vector in Cartesian coordinates using [Double] values. */
 data class Double3(val x: Double, val y: Double, val z: Double)
