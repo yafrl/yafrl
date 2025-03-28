f4d2f0deb6427d9487504d2c10d316092e16bd8e
Nathan Bedell <nbedell@tulane.edu>
2025-03-27 21:37:02 -0400

    Add tests for the optics integrations.

--- optics/build.gradle.kts
+++ optics/build.gradle.kts
@@ -3,9 +3,8 @@ extra["projectDescription"] =
 
 plugins {
     kotlin("multiplatform") version "2.1.10"
+    id("com.google.devtools.ksp") version "2.1.10-1.0.30"
     id("org.jetbrains.kotlinx.atomicfu") version "0.27.0"
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.10"
-    id("org.jetbrains.compose") version "1.7.3"
     id("com.vanniktech.maven.publish") version "0.31.0"
     id("org.jetbrains.kotlinx.kover") version "0.9.1"
     id("org.jetbrains.dokka") version "2.0.0"
@@ -28,10 +27,14 @@ kotlin {
                 implementation("io.arrow-kt:arrow-optics:2.0.1")
             }
         }
-        val jvmTest by getting {
+        val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
             }
         }
     }
+}
+
+dependencies {
+    ksp("io.arrow-kt:arrow-optics-ksp-plugin:2.0.1")
 }
