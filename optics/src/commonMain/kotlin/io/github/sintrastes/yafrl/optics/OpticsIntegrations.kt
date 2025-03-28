3dbe1a0a951be105bf008f9dbf87cfceabbb7d8b
Nathan Bedell <nbedell@tulane.edu>
2025-03-27 21:15:29 -0400

    Add a focus operator for binding states.

--- optics/src/commonMain/kotlin/io/github/sintrastes/yafrl/optics/OpticsIntegrations.kt
+++ optics/src/commonMain/kotlin/io/github/sintrastes/yafrl/optics/OpticsIntegrations.kt
@@ -2,8 +2,11 @@ package io.github.sintrastes.yafrl.optics
 
 import arrow.optics.Lens
 import arrow.optics.Prism
+import io.github.sintrastes.yafrl.BindingState
 import io.github.sintrastes.yafrl.Event
 import io.github.sintrastes.yafrl.State
+import io.github.sintrastes.yafrl.annotations.FragileYafrlAPI
+import io.github.sintrastes.yafrl.bindingState
 
 /** Embed an event in a larger set of events via a [Prism]. */
 fun <A, B> Event<A>.embed(inclusion: Prism<B, A>): Event<B> {
@@ -12,10 +15,28 @@ fun <A, B> Event<A>.embed(inclusion: Prism<B, A>): Event<B> {
     }
 }
 
-/** Focs on a smaller part of a [State] by applying a [Lens]. */
+/** Focus on a smaller part of a [State] by applying a [Lens]. */
 fun <A, B> State<A>.focus(lens: Lens<A, B>): State<B> {
     return map {
         lens.get(it)
     }
+}
+
+/**
+ * Focus on a smaller part of a [BindingState] by applying a [Lens]
+ **/
+@OptIn(FragileYafrlAPI::class)
+fun <A, B> BindingState<A>.focus(lens: Lens<A, B>): BindingState<B> {
+    val state = bindingState(lens.get(value))
+
+    this.collectSync { newValue ->
+        state.value = lens.get(newValue)
+    }
+
+    state.collectSync { newValue ->
+        value = lens.set(value, newValue)
+    }
+
+    return state
 }
 
