34dabc6a5092b2a371e4a288a76be0385f88978d
Nathan Bedell <nbedell@tulane.edu>
2025-04-15 20:38:01 -0400

    Add more vector utilities.

--- core/src/commonMain/kotlin/io/github/sintrastes/yafrl/vector/Vectors.kt
+++ core/src/commonMain/kotlin/io/github/sintrastes/yafrl/vector/Vectors.kt
@@ -1,10 +1,28 @@
 package io.github.sintrastes.yafrl.vector
 
 /** A two-dimensional vector in Cartesian coordinates using [Float] values. */
-data class Float2(val x: Float, val y: Float)
+data class Float2(val x: Float, val y: Float) {
+    companion object {
+        val zero = Float2(0f, 0f)
+
+        val x = Float2(1f, 0f)
+
+        val y = Float2(0f, 1f)
+    }
+}
 
 /** A three-dimensional vector in Cartesian coordinates using [Float] values. */
-data class Float3(val x: Float, val y: Float, val z: Float)
+data class Float3(val x: Float, val y: Float, val z: Float) {
+    companion object {
+        val zero = Float3(0f, 0f, 0f)
+
+        val x = Float3(1f, 0f, 0f)
+
+        val y = Float3(0f, 1f, 0f)
+
+        val z = Float3(0f, 0f, 1f)
+    }
+}
 
 /** A two-dimensional vector in Cartesian coordinates using [Double] values. */
 data class Double2(val x: Double, val y: Double)
