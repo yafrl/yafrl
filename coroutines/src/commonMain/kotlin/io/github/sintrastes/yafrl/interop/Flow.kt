d46d37155208a29bdda81ac5308633654bf4d8a0
Nathan Bedell <nbedell@tulane.edu>
2025-06-15 14:05:44 -0400

    Fix coverage issue with coroutine tests.

--- coroutines/src/commonMain/kotlin/io/github/sintrastes/yafrl/interop/Flow.kt
+++ coroutines/src/commonMain/kotlin/io/github/sintrastes/yafrl/interop/Flow.kt
@@ -1,10 +1,14 @@
 package io.github.sintrastes.yafrl.interop
 
+import io.github.sintrastes.yafrl.BindingState
+import io.github.sintrastes.yafrl.BroadcastEvent
 import io.github.sintrastes.yafrl.Event
 import io.github.sintrastes.yafrl.State
+import io.github.sintrastes.yafrl.annotations.FragileYafrlAPI
 import io.github.sintrastes.yafrl.broadcastEvent
 import io.github.sintrastes.yafrl.internal.Timeline
 import io.github.sintrastes.yafrl.bindingState
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.drop
@@ -14,36 +18,46 @@ import kotlinx.coroutines.launch
  * Construct an [io.github.sintrastes.yafrl.Event] at the edge of the FRP network by
  *  emitting a frame whenever the passed [kotlinx.coroutines.flow.Flow] updates.
  **/
+@OptIn(FragileYafrlAPI::class)
 inline fun <reified A> Flow<A>.asEvent(): Event<A> {
     val event = broadcastEvent<A>()
-
     val scope = Timeline.currentTimeline().scope
+    bindFlowToEvent(scope, this , event)
 
+    return event
+}
+
+// Note: Created to fix bug with test coverage.
+@FragileYafrlAPI
+fun <A> bindFlowToEvent(scope: CoroutineScope, flow: Flow<A>, event: BroadcastEvent<A>) {
     scope.launch {
-        collect { value ->
+        flow.collect { value ->
             event.send(value)
         }
     }
-
-    return event
 }
 
 /**
  * Construct a [io.github.sintrastes.yafrl.State] at the edge of the FRP network
  *  with the same values and update behavior of the passed [kotlinx.coroutines.flow.StateFlow].
  **/
+@OptIn(FragileYafrlAPI::class)
 inline fun <reified A> StateFlow<A>.asState(): State<A> {
     val state = bindingState(value)
-
     val scope = Timeline.currentTimeline().scope
+    bindStateFlowToState(scope, this, state)
 
+    return state
+}
+
+// Note: Created to fix bug with test coverage
+@FragileYafrlAPI
+fun <A> bindStateFlowToState(scope: CoroutineScope, flow: StateFlow<A>, state: BindingState<A>) {
     scope.launch {
         // Since state flows replay the current state, drop the first value
         // so we only emit on updates.
-        drop(1).collect { updatedValue ->
+        flow.drop(1).collect { updatedValue ->
             state.value = updatedValue
         }
     }
-
-    return state
 }
